dd965153938961365dafcc7dc748f6526d1866f7
3meee <vesninap@mail.ru>
2024-04-28 22:25:11 +0300

    fff

--- app/src/main/java/ru/polisha/zhest/MainActivity.kt
+++ app/src/main/java/ru/polisha/zhest/MainActivity.kt
@@ -4,23 +4,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
         val view = findViewById<View>(R.id.main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+
 
         val startButton = view.findViewById<Button>(R.id.startButton)
         val endButton = view.findViewById<Button>(R.id.endButton)
@@ -29,14 +35,15 @@ class MainActivity : AppCompatActivity() {
         startButton.isEnabled = false
         endButton.isEnabled = false
 
+        val icon = view.findViewById<ImageView>(R.id.icon)
+        icon.setOnClickListener{
+            endButton.isVisible = !endButton.isVisible
+        }
+
         val buttonClickToast = Toast.makeText(this, "You are ready", Toast.LENGTH_SHORT)
 
         endButton.setOnClickListener{
-            if (startButton.isEnabled){
-                startButton.isEnabled = false
-            }
-            else
-                startButton.isEnabled = true
+            startButton.isEnabled = !startButton.isEnabled
         }
 
         startButton.setOnClickListener{
